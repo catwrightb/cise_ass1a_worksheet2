e7df4b74a028a0f3ad0bf2bd0e298c825e59bff8
catwrightb <78883365+catwrightb@users.noreply.github.com>
2022-03-13 14:04:31 +1300

    updated test.js

--- frontend/frontend/src/App.test.js
+++ frontend/frontend/src/App.test.js
@@ -8,9 +8,8 @@ import React from 'react';
       ReactDOM.unmountComponentAtNode(div);
     });
 
-
     describe('Addition', () => {
       it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(4);
+        expect(2 + 2).toBe(5);
       });
     });
